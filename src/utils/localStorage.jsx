f1aac7df1740b791393f59a97f36b6eb68c6adfb
Anurag Bansal <devanuragbansal@gmail.com>
2024-12-31 21:40:35 +0530

    minor changes

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,15 +1,19 @@
 import data from '../data/data.json';
 
-function setLocalStorage(){
-    localStorage.setItem('employees', JSON.stringify(data.employees));
-    localStorage.setItem('admin', JSON.stringify(data.admin));
-}
+const setLocalStorage = () => {
+  localStorage.setItem('employees', JSON.stringify(data.employees));
+  localStorage.setItem('admin', JSON.stringify(data.admin));
+};
 
-function getLocalStorage(){
-    const employees = JSON.parse(localStorage.getItem('employees'));
-    const admin = JSON.parse(localStorage.getItem('admin'));
+const getLocalStorage = () => {
+  if (!localStorage.getItem('employees') || !localStorage.getItem('admin')) {
+    setLocalStorage();
+  }
 
-    return { employees, admin };
-}
+  const employees = JSON.parse(localStorage.getItem('employees'));
+  const admin = JSON.parse(localStorage.getItem('admin'));
+
+  return { employees, admin };
+};
 
 export { setLocalStorage, getLocalStorage };
